d75bac14aa419f1411a0300ccb487c58e8add409
nimisaya <nimisaya@outlook.com>
2021-01-05 22:43:49 +1100

    Render placeholder text on flight page

---
+++ src/components/Flight.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Flight extends React.Component {
+
+    render() {
+        return(
+            <h1>Book seat page</h1>
+            // Display flight details
+                // Date
+                // Flight number
+                // Origin -> Destination
+                // 
+
+            // Form for seat selection
+                // Display seats
+                    // Row labels
+                    // Column labels
+                    // Seats
+                        // Available
+                        // Unavailable
+                        // Selected
+
+                // Seat selection
+                // Submit button
+
+        ); // return
+    } // render
+} // class Flight
+
+export default Flight;
