be78c0df4ed1c7b6713df552d60be3b067910342
nimisaya <nimisaya@outlook.com>
2021-01-06 00:42:12 +1100

    Add planning notes to search component

--- src/components/search.js
+++ src/components/search.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
 class Search extends React.Component {
+
+    // state storing the origin and destination entered in the search form
+
+    // handleChangeOrigin
+    // Sets state of origin query
+
+    // handleChangeDestination
+    // Sets state of destination query
+
+    // handleSubmit method
+    // prevents page from reloading on form submission
+    // send search query (origin and destination) to parent component (Home)
+
     render(){
         return(
             <h1>Placeholder for flight search form</h1>
+
+            // Search form
+                // input for origin
+                // input for destination
+                // Search button
+
         ); // return
     } // render
 } // class Search
