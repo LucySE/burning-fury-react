7b2dfd3c1a6a4cb49acaf3d988a84a2073cb749d
nimisaya <nimisaya@outlook.com>
2021-01-05 23:08:05 +1100

    Create component for Search form

---
+++ src/components/search.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Search extends React.Component {
+    render(){
+        return(
+            <h1>Placeholder for flight search form</h1>
+        ); // return
+    } // render
+} // class Search
+
+export default Search;
