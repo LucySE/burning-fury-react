63ab74dbb6f2a508195a2bb0d5ef772e0f183a30
nimisaya <nimisaya@outlook.com>
2021-01-06 09:38:42 +1100

    Add planning notes to SearchResults component

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 class SearchResults extends React.Component {
+
     render(){
         return(
-            <h1>Placeholder for flight search results</h1>
+            <div>
+                <h1>Placeholder for flight search results</h1>
+                {/* 
+                    - Display list of flights based on search query
+                    - Each flight links to a flight page
+                        - ul that maps the flights to an li with a key=flight.id
+                        - each li contains a 'Link to' with the flight route = /flight/:flightID
+                 */}
+            </div>
         ); // return
     } // render
 } // class SearchResults
