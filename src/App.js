d36cafc23236db5b96db483d0118bd62f31e5241
Lucy Wirth <lucywirth@gmail.com>
2021-01-05 21:00:25 +1100

    Added components folder, flight and search pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Route, Link, HashRouter as Router  } from 'react-router-dom';
+
+
+class App extends React.Component {
+
+  render(){
+
+    return (
+      <div className="App">
+        <h1>Welcome to Burning Fury Airlines</h1>
+        <h2>Don't let the name put you off...</h1>
+
+        <Router>
+          <nav>
+            <Link to="/">Home</Link> |
+            <Link to="/search">Search Flights</Link> |
+            <Link to="/teethshop">Teeth Sales</Link>
+            <br />
+            <Route path="/" component={ SearchForm } /> { /* appears on every route */ }
+          </nav>
+          <hr />
+
+          {
+            // Like Rails routes.rb:
+            // get '/' => 'pages#home'
+          }
+          <Route exact path="/" component={ Home } />
+          <Route exact path="/procedures" component={ Procedures } />
+          <Route exact path="/teethshop" component={ TeethSales } />
+          <Route exact path="/search/:query" component={ SearchResults } />
+
+        </Router>
+
+        <footer>
+          <hr />
+          &copy; 2021 Unsettling Professionals
+        </footer>
+
+      </div>
+    );
+
+  } // render()
+
+} // class App
 
 export default App;
