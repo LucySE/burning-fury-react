29fc6521abb28fa0a596148f1e79d1dc595fb775
nimisaya <nimisaya@outlook.com>
2021-01-05 23:14:34 +1100

    Remove unused App component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import Home from './components/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
