45d3263cfe2faa0082fef76b9352d6c5f2ce4fd0
Lucy Wirth <lucywirth@gmail.com>
2021-01-06 12:21:39 +1100

    Updates to Search

--- src/components/search.js
+++ src/components/search.js
@@ -1,30 +1,44 @@
 import React from 'react';
+import axios from 'axios';
+import FlightInformation from './FlightInformation';
+import Flight from './Flight';
+
+
 
 class Search extends React.Component {
-
-    // state storing the origin and destination entered in the search form
-
+      // state storing the origin and destination entered in the search form
+    constructor(){
+      super();
+      this.state = {
+      origin: '',
+      destination: ''
+      }
+    }
     // handleChangeOrigin
     // Sets state of origin query
-
+    this.handleChangeOrigin = this.handleChangeOrigin.bind(this)
     // handleChangeDestination
     // Sets state of destination query
+    this.handleChangeDestination = this.handleChangeDestination.bind(this);
+    }
+      // handleSubmit method
+    this.handleSubmit = this.handleSubmit.bind(this);
 
-    // handleSubmit method
-    // prevents page from reloading on form submission
-    // send search query (origin and destination) to parent component (Home)
+      // prevents page from reloading on form submission
+    handleChangeOrigin =
+      // send search query (origin and destination) to parent component (Home)
 
-    render(){
-        return(
-            <h1>Placeholder for flight search form</h1>
+      render(){
+          return(
+              <h1>Placeholder for flight search form</h1>
 
-            // Search form
-                // input for origin
-                // input for destination
-                // Search button
+              // Search form
+                  // input for origin
+                  // input for destination
+                  // Search button
 
-        ); // return
-    } // render
-} // class Search
+          ); // return
+      } // render
+    } // class Search
 
 export default Search;
