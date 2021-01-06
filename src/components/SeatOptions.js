bedfc5dfd09b823eff2e0138f649c43ca8db11a4
nimisaya <nimisaya@outlook.com>
2021-01-06 22:17:21 +1100

    Clean up commenting on SeatOptions

--- src/components/SeatOptions.js
+++ src/components/SeatOptions.js
@@ -8,7 +8,7 @@ const SeatOptions = (props) => {
     const createColumnLabels = () => {
         let labels = [];
 
-        // Put an empty string for the first cell. Prevents the row labels from having a column label.
+        // Set first cell as empty. Prevents the row labels from having a column label.
         labels.push(<th> </th>)
 
         // Create a label for each column by looping through the total number of columns and converting the numbers to their alphabetical equivalent. Push each onto a label array
@@ -16,34 +16,37 @@ const SeatOptions = (props) => {
             labels.push(<th>{String.fromCharCode(i + 1 + 64)}</th>);
         }
         return labels;
-    }
+    } // createColumnLabels()
 
     const createSeats = () => {
         // Create an array to store the seats
         let seats = []
 
-        // Create a seat for each
+        // Create a seat for each row/col
         for (let i = 0; i < rows; i++){
-            let children = []
+            let seat = []
 
             // Add row label
-            children.push(<th>{i + 1}</th>)
+            seat.push(<th>{i + 1}</th>)
 
+            // Add seats to an individual row
             for (let j = 0; j < columns; j++){
                 if (i === 2 && j === 2){
-                    children.push(<td>{`X`}</td>)
+                    seat.push(<td>{`X`}</td>)
                 } else {
-
-                    children.push(<td>{`C${j + 1}`}</td>)
+                    seat.push(<td>{`C${j + 1}`}</td>)
                 }
-            }
-            seats.push(<tr>{children}</tr>)
-        }
+            } // for (j)
+
+            // Add the row to the array of seats
+            seats.push(<tr>{seat}</tr>)
+        }// for (i)
+
         return seats;
-    }
+    } // createSeats()
 
     return(
-
+        // Table showing seats on the plane
         <table>
             <thead>
                 <tr>
