f7be982fd03e61fc9d5c7ada059b6fbacb39ef99
nimisaya <nimisaya@outlook.com>
2021-01-06 22:59:40 +1100

    Convert SeatOptions to class component

--- src/components/SeatOptions.js
+++ src/components/SeatOptions.js
@@ -1,41 +1,48 @@
 import React from 'react';
 
-const SeatOptions = (props) => {
+class SeatOptions extends React.Component {
 
-    const rows = props.rows;
-    const columns = props.columns;
+    rows = this.props.rows;
+    columns = this.props.columns;
 
-    const createColumnLabels = () => {
+    createColumnLabels = () => {
         let labels = [];
 
         // Set first cell as empty. Prevents the row labels from having a column label.
         labels.push(<th> </th>)
 
         // Create a label for each column by looping through the total number of columns and converting the numbers to their alphabetical equivalent. Push each onto a label array
-        for (let i = 0; i < columns; i++){
+        for (let i = 0; i < this.columns; i++){
             labels.push(<th>{String.fromCharCode(i + 1 + 64)}</th>);
         }
         return labels;
     } // createColumnLabels()
 
-    const createSeats = () => {
+    createSeats = () => {
         // Create an array to store the seats
         let seats = []
 
         // Create a seat for each row/col
-        for (let i = 0; i < rows; i++){
+        for (let i = 0; i < this.rows; i++){
             let seat = []
 
             // Add row label
             seat.push(<th>{i + 1}</th>)
 
             // Add seats to an individual row
-            for (let j = 0; j < columns; j++){
+            for (let j = 0; j < this.columns; j++){
                 if (i === 2 && j === 2){
                     seat.push(<td>{`X`}</td>)
                 } else {
                     // seat.push(<td>{`C${j + 1}`}</td>)
-                    seat.push(<td><button onClick={function() { alert(String.fromCharCode(j + 1 + 64) + (i+1)); }}>O</button></td>)
+                    seat.push(
+                    <td>
+                        <button 
+                            onClick={() => { alert(String.fromCharCode(j + 1 + 64) + (i+1)); }}>
+                            O
+                            {/* {props.square} */}
+                        </button>
+                    </td>)
                 }
             } // for (j)
 
@@ -46,20 +53,22 @@ const SeatOptions = (props) => {
         return seats;
     } // createSeats()
 
-    return(
-        // Table showing seats on the plane
-        <table>
-            <thead>
-                <tr>
-                    {createColumnLabels()}
-                </tr>
-            </thead>
-            <tbody>
-                {createSeats()}
-            </tbody>
-        </table>
-    ); // return
-} // SeatOptions
+    render() {
+        return(
+            // Table showing seats on the plane
+            <table>
+                <thead>
+                    <tr>
+                        {this.createColumnLabels()}
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.createSeats()}
+                </tbody>
+            </table>
+        ); // return
+    }
+} // class SeatOptions
 
 // Selected option will need to be sent to the flight page (parent) via a function
 
