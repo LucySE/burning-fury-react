af417c16d9242d579027fa8b4f74649f20a9cc8f
nimisaya <nimisaya@outlook.com>
2021-01-06 16:30:27 +1100

    Retrieve plane info from server

--- src/components/Flight.js
+++ src/components/Flight.js
@@ -1,17 +1,34 @@
+import axios from 'axios';
 import React from 'react';
 import FlightInformation from './FlightInformation.js'
+
+const BASE_URL = 'http://localhost:3000/';
 
 class Flight extends React.Component {
 
     state = {
+        airplane: {},
         // Hardcoded flight information. 
         flight: {
             number: 'BA123',
             date: '2021-01-25',
             origin: 'Melbourne',
-            destination: 'Sydney'
+            destination: 'Sydney',
+            planeID: 2
         },
     }
+
+    // Fetch the number of rows and columns on the plane
+    fetchPlane = () => {
+        const planeURL = BASE_URL + 'airplanes/' + this.state.flight.planeID
+        axios.get(planeURL)
+        .then((res) => {
+            console.log('plane response:', res.data);
+        })
+        .catch(console.warn())
+    }
+
+    // fetchUsers()
 
     // fetchReservations() 
     // will be used in componentDidMount to fetch the initial seat availability then poll the rails server and fetch any changes to the avialability
@@ -19,6 +36,10 @@ class Flight extends React.Component {
     // componentDidMount()
     // Runs as soon as component loads
     // will fetchReservations()
+    componentDidMount(){
+        console.log('mounted!');
+        this.fetchPlane();
+    }
 
     // saveReservation()
     // will save the users reservation 
@@ -46,7 +67,11 @@ class Flight extends React.Component {
                             // Available
                             // Unavailable
                             // Selected */}
-            
+
+                            {/* display rows and columns */}
+                            {/* <p>Rows: {this.state.airplane.rows}</p>
+                            <p>Columns: {this.state.airplane.columns}</p>
+             */}
 
                 {/* // Seat selection
                 // Submit button */}
