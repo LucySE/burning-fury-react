4316fc7cd3845760f39a33d357ca3216cf35a5b6
nimisaya <nimisaya@outlook.com>
2021-01-06 22:07:17 +1100

    Display basic seat layout with labels

--- src/components/SeatOptions.js
+++ src/components/SeatOptions.js
@@ -5,13 +5,37 @@ const SeatOptions = (props) => {
     const rows = props.rows;
     const columns = props.columns;
 
+    const createColumnLabels = () => {
+        let labels = [];
+
+        // Put an empty string for the first cell. Prevents the row labels from having a column label.
+        labels.push(<th> </th>)
+
+        // Create a label for each column by looping through the total number of columns and converting the numbers to their alphabetical equivalent. Push each onto a label array
+        for (let i = 0; i < columns; i++){
+            labels.push(<th>{String.fromCharCode(i + 1 + 64)}</th>);
+        }
+        return labels;
+    }
+
     const createSeats = () => {
+        // Create an array to store the seats
         let seats = []
 
+        // Create a seat for each
         for (let i = 0; i < rows; i++){
             let children = []
+
+            // Add row label
+            children.push(<th>{i + 1}</th>)
+
             for (let j = 0; j < columns; j++){
-                children.push(<td>{`C${j + 1}`}</td>)
+                if (i === 2 && j === 2){
+                    children.push(<td>{`X`}</td>)
+                } else {
+
+                    children.push(<td>{`C${j + 1}`}</td>)
+                }
             }
             seats.push(<tr>{children}</tr>)
         }
@@ -21,7 +45,14 @@ const SeatOptions = (props) => {
     return(
 
         <table>
-            {createSeats()}
+            <thead>
+                <tr>
+                    {createColumnLabels()}
+                </tr>
+            </thead>
+            <tbody>
+                {createSeats()}
+            </tbody>
         </table>
 
     ); // return
