c4df50bacf7214c054554d346f9c09db1c61d054
nimisaya <nimisaya@outlook.com>
2021-01-06 11:03:13 +1100

    Display hardcoded flight info

--- src/components/Flight.js
+++ src/components/Flight.js
@@ -6,6 +6,16 @@ import FlightInformation from './FlightInformation.js'
 
 class Flight extends React.Component {
 
+    state = {
+        // Hardcoded flight information. 
+        flight: {
+            number: 'BA123',
+            date: '2021-01-25',
+            origin: 'Melbourne',
+            destination: 'Sydney'
+        },
+    }
+
     // fetchReservations() 
     // will be used in componentDidMount to fetch the initial seat availability then poll the rails server and fetch any changes to the avialability
 
@@ -20,25 +30,25 @@ class Flight extends React.Component {
     render() {
         return(
             <div>
-            <h1>Book seat page</h1>
-            {/* // Display flight details
-                    // Date
-                    // Flight number
-                    // Origin -> Destination 
+                <h2>Book seat</h2>
+                {/* // Display flight details
+                        // Date
+                        // Flight number
+                        // Origin -> Destination 
 
-                Flight details passed to FlightInformation which is a Functional Component. It returns the flight info for rendering.
-            */}
-                <FlightInformation/>
-                {/* <FlightInformation flightNumber={} /> */}
-{/* 
-            // Form for seat selection (Might be its own component?)
-                // Display seats
-                    // Row labels
-                    // Column labels
-                    // Seats
-                        // Available
-                        // Unavailable
-                        // Selected */}
+                    Flight details passed to FlightInformation which is a Functional Component. It returns the flight info for rendering.
+                */}
+                <FlightInformation {...this.state.flight} />
+
+                {/* 
+                // Form for seat selection (Might be its own component?)
+                    // Display seats
+                        // Row labels
+                        // Column labels
+                        // Seats
+                            // Available
+                            // Unavailable
+                            // Selected */}
             
 
                 {/* // Seat selection
--- src/components/FlightInformation.js
+++ src/components/FlightInformation.js
@@ -3,12 +3,7 @@ import React from 'react';
 // Returns information about a Flight for rendering
 const FlightInformation = (props) => {
     return(
-        <div>
-            {/* This is placeholder until we have the data to display. Refer commented out code below */}
-            <p>Date Flight# Origin &lt; Destination</p>
-            
-            {/* <p>{props.date} {props.flightNumber} {props.origin} {props.destination} </p> */}
-        </div>
+        <p>{props.date} {props.number} {props.origin} &gt; {props.destination}</p>
     ); // return
 } // FlightInformation
 
