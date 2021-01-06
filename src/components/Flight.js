ba78ef6108d43b86d3ebc78947b39d5bc6481923
nimisaya <nimisaya@outlook.com>
2021-01-06 20:59:32 +1100

    Render SeatOptions table

--- src/components/Flight.js
+++ src/components/Flight.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React from 'react';
 import FlightInformation from './FlightInformation.js'
+import SeatOptions from './SeatOptions.js'
 
 const BASE_URL = 'http://localhost:3000/';
 
@@ -19,7 +20,7 @@ class Flight extends React.Component {
         },
     }
 
-    // Fetch the number of rows and columns on the plane
+    // Fetch the number of rows and columns on the plane and update their state
     fetchPlane = () => {
         const planeURL = BASE_URL + 'airplanes/' + this.state.flight.planeID
         axios.get(planeURL)
@@ -36,9 +37,7 @@ class Flight extends React.Component {
     // fetchReservations() 
     // will be used in componentDidMount to fetch the initial seat availability then poll the rails server and fetch any changes to the avialability
 
-    // componentDidMount()
-    // Runs as soon as component loads
-    // will fetchReservations()
+    // Runs as soon as component loads and fetches information about the flight and it's reservations from the server
     componentDidMount(){
         console.log('mounted!');
         this.fetchPlane();
@@ -52,14 +51,11 @@ class Flight extends React.Component {
         return(
             <div>
                 <h2>Book seat</h2>
-                {/* // Display flight details
-                        // Date
-                        // Flight number
-                        // Origin -> Destination 
-
-                    Flight details passed to FlightInformation which is a Functional Component. It returns the flight info for rendering.
-                */}
+                {/* Displays information about the flight. Flight details are passed to FlightInformation functional component and it returns the formatted flight info for rendering */}
                 <FlightInformation {...this.state.flight} />
+
+                {/* Display plane layout for seat selection */}
+                <SeatOptions rows={this.state.rows} columns={this.state.columns}/>
 
                 {/* 
                 // Form for seat selection (Might be its own component?)
@@ -74,6 +70,7 @@ class Flight extends React.Component {
                             {/* display rows and columns */}
                             <p>Rows: {this.state.rows}</p>
                             <p>Columns: {this.state.columns}</p>
+                        
             
 
                 {/* // Seat selection
